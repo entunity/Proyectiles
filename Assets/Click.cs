36ff9dcbdbceb42dc5bddd72e3a716414b58c881
enrique <enrique.tunity@gmail.com>
2018-10-19 13:48:04 +0200

    Initial commit

---
+++ Assets/Click.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDisparo : MonoBehaviour {
+
+    [SerializeField] Transform bomba;
+    [SerializeField] GameObject canion;
+    private GameObject disparo;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            disparo = Instantiate(bomba.gameObject, canion.transform.position, canion.transform.rotation);
+            disparo.GetComponent<Rigidbody>().AddRelativeForce(0, 100f, 100f);
+            
+        }
+	}
+    
+}
---
+++ Assets/_GameAssets/Explotar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explotar : MonoBehaviour {
+
+    [SerializeField] int radioExplosion=3;
+	// Use this for initialization
+	void Start () {
+        Invoke("explotar", 2f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void explotar() {
+        //this.GetComponent<Rigidbody>().AddExplosionForce(200, this.transform.position, 20f);
+        Collider[] cosas = Physics.OverlapSphere(this.transform.position, radioExplosion);
+        foreach(var cosa in cosas) {
+            if (cosa.tag == "Respawn") {
+                //cosa.GetComponent<Rigidbody>().AddRelativeForce(100, 100, 100);
+                cosa.GetComponent<Rigidbody>().AddExplosionForce(200, this.transform.position, 20f);
+                Debug.Log(cosa.gameObject.name);
+            }
+        }
+        Destroy(this.gameObject);
+    }
+}
